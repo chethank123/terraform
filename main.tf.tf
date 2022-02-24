0d33711082ccd52ee25e58bfb318a738bd6fb0dc
Keerthipti Chethan <chethank@positiveedge.com>
2022-02-24 10:45:05 +0530

    terraform scripts commit

---
+++ main.tf.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "chethanrg" {
+    name = "terraformrg"
+    location = "East US"
+}
---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+    subscription_id = "ca81862e-8828-4ef8-a19b-3c5161157823"
+    client_id = "184e14c0-5fcd-483f-beba-83508ba045ed"
+    client_secret = "M5F7Q~sWjiJN1vK_xV~UCg1TX8Jw00bYOSfFk"
+    tenant_id = "2cffdc58-95c5-4ab4-bb2d-ed4db3baafe7"
+     features{}
+}
+
+ 
